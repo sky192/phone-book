badf57997fc71fb4cda3afe1d68a3c218aae4924
Samaheh Loghmani <samahehloghmani@gmail.com>
2022-05-12 09:28:48 -0400

    added local storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddContact from "./components/AddContact";
 import ContactList from "./components/ContactList";
 
@@ -12,6 +12,17 @@ function App() {
             { id: Math.ceil(Math.random() * 100), ...contact },
         ]);
     };
+
+    // saving data in local storage
+    useEffect(() => {
+        const savedContacts = JSON.parse(localStorage.getItem("contacts"));
+        if (savedContacts) setContacts(savedContacts);
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("contacts", JSON.stringify(contacts));
+    }, [contacts]);
+
     return (
         <div className="App">
             <h1>Phone Book</h1>
